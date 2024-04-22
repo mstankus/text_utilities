7cce7d8dde2ef4be6437f14759bec24a631ba116
Mark Stankus] <mstankus@gmail.com>
2024-04-21 19:40:12 -0700

    Working on multiremover;still work in progress

--- src/bin/multiremover.rs
+++ src/bin/multiremover.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufReader,BufRead};
+use std::process::Command;
+use glob::glob;
 
 fn main() {
+  let args : Vec<_> = env::args().collect();
+  let str = format!("diff-{}-*",args[1]).to_string();
+  if let Some(Ok(path)) = glob(&str).expect("hi").next() {
+    let f = File::open(path.clone());
+    let mut b = BufReader::new(f.unwrap());
+    let mut s = String::new();
+    let _ = b.read_line(&mut s);
+    println!("item:{:?}",path);
+    println!("rm \"{}\"",&s[9..].trim());
+  }
+  println!("{:?}",Command::new("rm")
+      .arg(format!("diff-{}-*",args[1]).to_string())
+      .arg(format!("diff-*-{}-*",args[1]).to_string())
+      .output()
+      .expect("remove did not work!")
+      .stdout);
 }
+
